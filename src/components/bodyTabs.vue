f9d2134be6899266be358212c02f0b930c1e6ae6
TharothTHO <tharoth.tho@gmail.com>
2022-02-28 22:18:07 +0700

    Change component

---
+++ src/components/bodyTabs.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class='wrapper'>
+          <tabs :mode="mode">
+            <tab title="Week 1">Hello From Week 1</tab>
+            <tab title="Week 2">Hello From Week 2</tab>
+            <tab title="Week 3">Hello From Week 3</tab>
+            <tab title="Week 4">Hello From Week 4</tab>
+            <tab title="Week 5">Hello From Week 5</tab>
+            <tab title="Week 6">Hello From Week 6</tab>
+            <tab title="Week 7">Hello From Week 7</tab>
+            <tab title="Week 8">Hello From Week 8</tab>
+            <tab title="Week 9">Hello From Week 7</tab>
+            <tab title="Week 10">Hello From Week 8</tab>
+          </tabs>
+    </div>
+</template>
+
+<script>
+import Tab from './Tab.vue';
+import Tabs from './Tabs.vue'
+export default {
+  components: {Tab , Tabs},
+  data () {
+    return {
+      mode: 'dark'
+    }
+  },
+  methods: {
+    changeStyle () {
+      if (this.mode === 'dark') {
+        this.mode = 'light'
+      } else {
+        this.mode = 'dark'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.wrapper {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    padding-top: 50px;
+    color: #203E61;
+  }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,43 +2,19 @@
   <div class="home">
     <ul>   
       <navigation />
-      <div class='wrapper'>
-          <tabs :mode="mode">
-            <tab title="Week 1">Hello From Week 1</tab>
-            <tab title="Week 2">Hello From Week 2</tab>
-            <tab title="Week 3">Hello From Week 3</tab>
-            <tab title="Week 4">Hello From Week 4</tab>
-            <tab title="Week 5">Hello From Week 5</tab>
-            <tab title="Week 6">Hello From Week 6</tab>
-            <tab title="Week 7">Hello From Week 7</tab>
-            <tab title="Week 8">Hello From Week 8</tab>
-            <tab title="Week 9">Hello From Week 7</tab>
-            <tab title="Week 10">Hello From Week 8</tab>
-          </tabs>
-        </div>
-      </ul>
+      <bodyTabs />
+    </ul>
   </div>
 </template>
 
 <script>
 import navigation from '../components/navigation.vue';
-import Tab from '../components/Tab.vue';
-import Tabs from '../components/Tabs.vue'
+import bodyTabs from '../components/bodyTabs.vue'
 export default {
   name: "Home",
-  components: {navigation , Tab , Tabs},
+  components: {navigation , bodyTabs},
   data () {
     return {
-      mode: 'dark'
-    }
-  },
-  methods: {
-    changeStyle () {
-      if (this.mode === 'dark') {
-        this.mode = 'light'
-      } else {
-        this.mode = 'dark'
-      }
     }
   }
 };
@@ -51,12 +27,6 @@ export default {
   padding: ;
     width: 150px;
 }
-  .wrapper {
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    padding-top: 50px;
-    color: #203E61;
-  }
+
 
 </style>
